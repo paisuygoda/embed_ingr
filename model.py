e9ab7384bad1ffeec807bd930e877cd009da7d77
paisuygoda <paisuygoda3@gmail.com>
2018-02-28 10:42:30 +0900

    Add test to multilabel model

--- model.py
+++ model.py
@@ -84,11 +84,10 @@ class MultilabelModel(nn.Module):
 
         image_model = models.resnet50()
         image_model.fc = nn.Linear(2048, 469)
-        if trainmode:
-            image_model = torch.nn.DataParallel(image_model).cuda()
-            checkpoint = torch.load(
-                "model/ResNet50_469_best.pth.tar")  # FoodLog-finetuned single-class food recognition model
-            image_model.load_state_dict(checkpoint["state_dict"])
+        image_model = torch.nn.DataParallel(image_model).cuda()
+        checkpoint = torch.load(
+            "model/ResNet50_469_best.pth.tar")  # FoodLog-finetuned single-class food recognition model
+        image_model.load_state_dict(checkpoint["state_dict"])
 
         image_model.module.fc = nn.Linear(2048, opts.numofingr)
         self.image_model = image_model.cuda()
